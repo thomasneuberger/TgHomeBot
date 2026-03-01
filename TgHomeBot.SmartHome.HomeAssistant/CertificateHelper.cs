b3db576de4bb110bfaf5178b210a8862ffa19dfa
Thomas Neuberger <23504477+thomasneuberger@users.noreply.github.com>
2026-03-01 16:16:41 +0100

    Fix loading root certificate

--- TgHomeBot.SmartHome.HomeAssistant/CertificateHelper.cs
+++ TgHomeBot.SmartHome.HomeAssistant/CertificateHelper.cs
@@ -6,28 +6,6 @@ internal static class CertificateHelper
 {
     internal static X509Certificate2 LoadCertificate(string path)
     {
-        const int maxScanLines = 50;
-        var isPem = false;
-
-        using (var reader = new StreamReader(path))
-        {
-            string? line;
-            var linesRead = 0;
-
-            while (linesRead < maxScanLines && (line = reader.ReadLine()) is not null)
-            {
-                linesRead++;
-
-                if (line.Contains("-----BEGIN CERTIFICATE-----", StringComparison.Ordinal))
-                {
-                    isPem = true;
-                    break;
-                }
-            }
-        }
-
-        return isPem
-            ? X509Certificate2.CreateFromPemFile(path)
-            : X509CertificateLoader.LoadCertificateFromFile(path);
+        return X509CertificateLoader.LoadCertificateFromFile(path);
     }
 }
